0e9e6c8371af772983c944b93fb41fabe92fa308
Craig Ambrose <craig@craigambrose.com>
2012-05-11 19:51:48 +1200

    Empty rails app with backbone

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VillageHire::Application.config.secret_token = 'f8ff7f33055401164f656c336f9ff25962b1913dc7e53996b15d8a05d6ca7ff3df2f1fac538be32aa8d7262e6a7b69a1aa125138689293253b29fa2fd5aea56e'
